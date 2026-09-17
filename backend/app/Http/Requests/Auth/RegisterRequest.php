49b38d34dc238909bac04e4f4d05f8b901f823f1
artemkaraseni225 <artemkaraseni225@gmail.com>
2026-09-17 16:25:47 +0300

    feat(auth): modify registerrequest

--- backend/app/Http/Requests/Auth/RegisterRequest.php
+++ backend/app/Http/Requests/Auth/RegisterRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class RegisterRequest extends FormRequest
 {
@@ -17,14 +16,12 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', Rule::in(['customer'])],
-            'phone' => ['required_if:role,customer', 'nullable', 'string', 'max:30'],
-            'address' => ['required_if:role,customer', 'nullable', 'string', 'max:500'],
-            'date_of_birth' => ['required_if:role,customer', 'nullable', 'date'],
 
-            // Необязательно для broker по умолчанию 0, назначается позже админом
-            'commission_rate' => ['nullable', 'numeric', 'min:0', 'max:100'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+
+            'phone' => ['required', 'string', 'max:30'],
+            'address' => ['required', 'string', 'max:500'],
+            'date_of_birth' => ['required', 'date'],
         ];
     }
 }
